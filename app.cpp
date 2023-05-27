77037e62c8e5f266fc3521e9d69f3308495291a5
vladsendrix <vladsendrix@gmail.com>
2023-05-27 13:53:46 +0300

    remove tests for now
    will be rewritten later

--- app.cpp
+++ app.cpp
@@ -2,11 +2,8 @@
 #include "repository/Repository.h"
 #include "controller/Controller.h"
 #include "ui/Console.h"
-#include "test/Test.h"
 
 int main() {
-    Test test;
-    test.TestAll();
     std::shared_ptr<repository::Repository> scooters = std::make_shared<repository::Repository>();
     std::shared_ptr<controller::ProductController> controller = std::make_shared<controller::ProductController>();
     std::unique_ptr<ui::Console> app = std::make_unique<ui::Console>(controller);
